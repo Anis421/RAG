c2a6171a58caf4db1426ea6e881a438f334d048b
Anis421 <122622931+Anis421@users.noreply.github.com>
2025-03-12 15:54:37 +0545

    gui.py

--- gui.py
+++ gui.py
@@ -9,7 +9,6 @@ query = st.text_input("Enter a TV show or movie name: ")
 if st.button("Search"):
     movie_info = omdb(query)
     trailer_link = youtube_trailer(query)
-    ai_reply = rag_response(query)
 
     st.write("Movie Info")
     st.json(movie_info)
